a113dd461445572f8c3daf786b43f068aeeaf860
deepakmench18 <deepakmench18@gmail.com>
2021-06-23 12:17:54 +0530

    Calculated Line Lenght

---
+++ Line_comparison.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+public class Line_Comparison {
+
+	public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Enter x1");
+	   int x1 = sc.nextInt();
+	   System.out.println("Enter x2");
+	   int x2= sc.nextInt();
+	   System.out.println("Enter x3");
+	   int y1 = sc.nextInt();
+	   System.out.println("Enter x4");
+	   int y2 = sc.nextInt();
+	   System.out.println("Enter P1");
+	   int p1 = sc.nextInt();
+	   System.out.println("Enter p2");
+	   int p2 = sc.nextInt();
+	   System.out.println("Enter p3");
+	   int q1 = sc.nextInt();
+	   System.out.println("Enter p4");
+	   int q2 = sc.nextInt();
+	   int a = (x2-x1)*(x2-x1);
+	   int b = (y2-y1)*(y2-y1);
+	   int c = (p2-p1)*(p2-p1);
+	   int d = (q2-q1)*(q2-q1);
+       double length_line1 = Math.sqrt(a + b);
+       double length_line2 = Math.sqrt(c + d);
+	}
+
+}
